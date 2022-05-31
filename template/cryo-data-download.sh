b68fdcb784f5a9284c1a0a138864247a65a6c84c
Kenneth D. Mankoff <mankoff@gmail.com>
2022-05-31 14:01:15 -0700

    Remove dev files

--- template/cryo-data-download.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# can be more involved, e.g. wrapping download-url in a wget spyder
-# See also https://docs.datalad.org/en/stable/generated/man/datalad-addurls.html
-datalad download-url https://filesamples.com/samples/document/txt/sample1.txt
-
