a605fa96d165813140225511fd39e6719c21d698
Kenneth D. Mankoff <mankoff@gmail.com>
2022-05-31 16:41:58 -0700

    [DATALAD] Recorded changes

--- .bin/configure.sh
+++ .bin/configure.sh
@@ -1,1 +1,25 @@
-/annex/objects/SHA256E-s713--e737e954afe2e59f14be4481bb6947e762c78b48aa5f445ec40a19d020ad156b.sh
+#!/usr/bin/env bash
+
+# Make a cryo-data dataset a child of the database, and then of all relevant akas
+
+# load pre.sh
+DIR=$(dirname $(readlink -f $0))
+source ${DIR}/pre.sh
+
+# # make child of database
+# log_info "Configure database"
+# cd ./database
+# datalad clone -D $name https://github.com/cryo-data/${name}
+# datalad save -r
+# datalad push --to origin
+# cd $CWD
+
+# make child of all akas
+for key in $(yq '.aka | keys' ${dir}/cryo-data.yaml | cut -d" " -f2); do
+  val=$(yq ".aka.${key}" ${dir}/cryo-data.yaml)
+  if [[ ${val} == "" ]]; then continue; fi
+  log_info "Linking ${name} to ${key}/${val}"
+  datalad clone -D ${name} https://github.com/cryo-data/${name} ${key}/${val}
+  datalad save -r
+done
+datalad push -r --to origin
